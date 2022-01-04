e7778481dc4905a74eaaba53ecd457c6826d44df
Naruto Ishikawa <garypippi@gmail.com>
2022-01-05 04:31:28 +0900

    [LSP] add autoindent for intelephense

--- lua/pippi/lsp/intelephense.lua
+++ lua/pippi/lsp/intelephense.lua
@@ -1,3 +1,7 @@
 return function(lsp)
-    lsp.intelephense.setup({})
+    lsp.intelephense.setup({
+        on_attach = function (client, bufnr)
+            vim.api.nvim_buf_set_option(bufnr, 'autoindent', true)
+        end
+    })
 end
